f62dcca5f0adc419d3dc93e1ab5f854e267e95d6
Daniel Aguila <danielbaguila@gmail.com>
2024-11-10 03:01:32 -0600

    Bottom and Drawer Navigator

--- App.js
+++ App.js
@@ -1,5 +1,57 @@
 import { Text } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {Ionicons} from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import WelcomeScreen from './screens/WelcomeScreen'
+import UserScreen from './screens/UserScreen'
+
+const Drawer = createDrawerNavigator();
+const BottomTap = createBottomTabNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  //this makes the first screen 'User'
+  //the headerTintColor is basically the writing on the header
+  //Navigator passes down the color and size values to Screen
+  return (
+    // This is for the Drawer Navigator
+    //<NavigationContainer>
+    //  <Drawer.Navigator initialRouteName='User' screenOptions={{
+    //      headerStyle: {backgroundColor: '#3c0a6b'},
+    //      headerTintColor: 'white',
+    //      drawerActiveBackgroundColor: '#f0e1ff',
+    //      drawerActiveTintColor: '#4c0a6b',
+    //  }}>
+    //    <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
+    //      drawerLabel: 'Welcome Screen',
+    //      drawerIcon: ({color, size}) => 
+    //        <Ionicons name="home" color={color} size={size}/>
+    //    }}
+    //    />
+    //    <Drawer.Screen name="User" component={UserScreen} options={{
+    //      drawerIcon: ({color, size}) => (
+    //        <Ionicons name="person" color={color} size={size}/>
+    //      )
+    //    }}/>
+    //  </Drawer.Navigator>
+
+    //This is for Bottom Navigator
+    //</NavigationContainer>
+      <NavigationContainer>
+        <BottomTap.Navigator initialRouteName='User' screenOptions={{
+          headerStyle: {backgroundColor: '#3c0a6b'},
+          headerTintColor: 'white',
+          tabBarActiveTintColor: '#3c0a6b'
+      }}>
+          <BottomTap.Screen name="Welcome" component={WelcomeScreen} options={{
+            tabBarIcon: ({color, size}) => <Ionicons name="home" color={color} size={size}/>
+          }}
+          />
+          <BottomTap.Screen name="User" component={UserScreen} options={{
+            tabBarIcon: ({color, size}) => <Ionicons name="person" color={color} size={size}/>
+          }}/>
+      </BottomTap.Navigator>
+    </NavigationContainer>
+  );
 }
